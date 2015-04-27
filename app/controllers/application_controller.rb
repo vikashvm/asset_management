d544f7a3f98e2d65dcb5459989c7403320867a44
vikash vikram <vvikram@xebia.com>
2015-04-27 11:41:22 +0530

    rescuing from exception fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+  before_action :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = "*"
+    #headers['Access-Control-Allow-Credentials'] = "true"
+    headers['Access-Control-Request-Method'] = %w{GET POST PUT DELETE OPTIONS}.join(",")
+    headers['Access-Control-Allow-Headers']= "Authorization"
+  end
+
+  rescue_from Exception do |exception|
+    render json: {message: exception.message, success: false}, status: :internal_server_error
+  end
+
+
 end
